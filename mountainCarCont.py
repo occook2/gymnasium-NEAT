a7b21eee1c5c978d38d9c686e412f92931bec327
occook2 <119069061+occook2@users.noreply.github.com>
2023-12-10 15:23:43 -0500

    Add 2 more envs to folder (separate py and config)

--- mountainCarCont.py
+++ mountainCarCont.py
@@ -7,7 +7,7 @@ def eval_genomes(genomes, config):
     # Takes in set of genomes. For each genome, run the entire test then return its fitness
     
     # First set the environment
-    env = gym.make('CartPole-v1')
+    env = gym.make('MountainCar-v0')
 
     # Test setup and execution for each genome
     for genome_id, genome in genomes:
@@ -15,24 +15,30 @@ def eval_genomes(genomes, config):
         fitness = 0 # Set/reset fitness
         net = neat.nn.FeedForwardNetwork.create(genome, config) # Create NN from genome
         observation, _ = env.reset()
-        
+        max_right = observation[0]*50
+
         # Test Execution
         while 1:
             output = net.activate(observation) # Let NN determine next action
-            if output[0] >= 0: # Convert NN output to environment action
-                action = 1
-            else:
+            if output[0] < -0.2: # Convert NN output to environment action
                 action = 0
+            elif output[0] > 0.2:
+                action = 2
+            else:
+                action = 1
              
             # Move environment forward one step based on NN action
-            observation, reward, terminated, info, _ = env.step(action)
+            observation, reward, terminated, truncated, _ = env.step(action)
+            if observation[0] > max_right:
+                max_right = observation[0]*50
+
             fitness += reward # Add reward since one step was taken
 
-            if terminated: # Cart failed to balance poll - Reset environment for next genome and end test
+            if truncated or terminated: # Cart to   - Reset environment for next genome and end test
                 env.reset() # Fail-safe
                 break
         
-        genome.fitness = fitness # Set fitness to genome fitness
+        genome.fitness = fitness + max_right # Set fitness to genome fitness 
 
 
 def run(config_file):  
@@ -48,11 +54,10 @@ def run(config_file):
     p.add_reporter(stats)
 
     # Run for number of generations.
-    winner = p.run(eval_genomes, 5)
+    winner = p.run(eval_genomes, 100)
 
 if __name__ == '__main__':
     # Find the Config File for Neat, will be in the same folder
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'cartandpoleConfig.txt')
+    config_path = os.path.join(local_dir, 'mountainCarConfig.txt')
     run(config_path)
-
